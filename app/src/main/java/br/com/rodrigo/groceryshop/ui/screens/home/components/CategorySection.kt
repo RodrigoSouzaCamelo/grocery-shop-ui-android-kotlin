e23f70b7fae2e2e398b489721fae4efcfa716498
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-03-15 20:10:20 -0300

    create category button

--- app/src/main/java/br/com/rodrigo/groceryshop/ui/screens/home/components/CategorySection.kt
+++ app/src/main/java/br/com/rodrigo/groceryshop/ui/screens/home/components/CategorySection.kt
@@ -1,6 +1,10 @@
 package br.com.rodrigo.groceryshop.ui.screens.home.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
@@ -10,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.rodrigo.groceryshop.R
@@ -34,8 +39,26 @@ fun CategorySection() {
             modifier = Modifier.fillMaxWidth()
         ) {
             CategoryButton(
-                text = "",
+                text = "Fruits",
                 icon = painterResource(R.drawable.ic_orange),
+                backgroundColor = Color(0xFFFEF4E7)
+            )
+
+            CategoryButton(
+                text = "Vegetables",
+                icon = painterResource(R.drawable.ic_veg),
+                backgroundColor = Color(0xFFFEF4E7)
+            )
+
+            CategoryButton(
+                text = "Dairy",
+                icon = painterResource(R.drawable.ic_cheese),
+                backgroundColor = Color(0xFFFEF4E7)
+            )
+
+            CategoryButton(
+                text = "Meats",
+                icon = painterResource(R.drawable.ic_meat),
                 backgroundColor = Color(0xFFFEF4E7)
             )
         }
@@ -48,10 +71,31 @@ fun CategoryButton(
     icon: Painter,
     backgroundColor: Color
 ) {
+    Column(
+        modifier = Modifier
+            .width(64.dp)
+            .clickable { }
+    ) {
+        Box(
+            modifier = Modifier
+                .size(64.dp)
+                .background(
+                    color = backgroundColor,
+                    shape = RoundedCornerShape(12.dp)
+                ).padding(18.dp)
+        ) {
+            Image(painter = icon, contentDescription = null)
+        }
 
+        Text(
+            text = text,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun CategorySectionPreview() {
     GroceryShopTheme {
